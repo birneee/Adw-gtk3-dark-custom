ddfec5e960397c101fbd312a72dc8bbb51ae2a3c
Benedikt Spies <benedikt.spies@gmail.com>
2025-11-26 01:50:29 +0100

    nix build

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
+PREFIX ?= ~/.themes
+THEME_NAME = adw-gtk3-dark-custom
+INSTALL_PATH = $(PREFIX)/$(THEME_NAME)
+
 install:
-	mkdir -p ~/.themes
-	cp -Rf . ~/.themes/adw-gtk3-custom-dark
+	mkdir -p $(INSTALL_PATH)
+	cp -Rf ./gnome-shell $(INSTALL_PATH)/gnome-shell
+	cp -Rf ./gtk-3.0 $(INSTALL_PATH)/gtk-3.0
+	cp -Rf ./index.theme $(INSTALL_PATH)/index.theme
 
 use:
 	gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3-custom-dark"	
--- flake.nix
+++ flake.nix
@@ -3,20 +3,42 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let 
-          pkgs = import nixpkgs {
-            inherit system;
-          };
-        in 
-        { 
-          devShells.default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              watchexec
-            ];
-          };
-        }
-      );
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "adw-gtk3-dark-custom";
+          version = "0.1.0";
+          src = pkgs.lib.cleanSource ./.;
+          dontBuild = true;
+          strictDeps = true;
+          postPatch = ''
+            sed -i '/gtk-dark.css/d' gtk-3.0/gtk.css
+            sed -i '1i @import url("${pkgs.adw-gtk3}/share/themes/adw-gtk3/gtk-3.0/gtk-dark.css");' gtk-3.0/gtk.css
+          '';
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out/share/themes
+            make install PREFIX=$out/share/themes
+            runHook postInstall
+          '';
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            watchexec
+          ];
+        };
+      }
+    );
 }
